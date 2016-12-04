f29c74040b1d77b06b8e60016d21a835e78a20f0
Ryota Konno <r.konno@nowall.co.jp>
2016-12-05 01:47:23 +0900

    [add] article view

--- app/src/main/kotlin/xyz/yurue/nobu/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/xyz/yurue/nobu/qiitaclient/MainActivity.kt
@@ -2,11 +2,25 @@ package xyz.yurue.nobu.qiitaclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import xyz.yurue.nobu.qiitaclient.model.Article
+import xyz.yurue.nobu.qiitaclient.model.User
+import xyz.yurue.nobu.qiitaclient.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // ArticleViewオブジェクトを生成
+        val articleView = ArticleView(applicationContext)
+
+        // Articleオブジェクトを生成して、ArticleViewオブジェクトにセット
+        articleView.setArticle(Article(id = "123",
+                title = "Kotlin ことはじめ",
+                url = "http://www.example.com/articles/123",
+                user = User(id = "456", name = "konnobu", profileImageUrl = "")))
+
+        // このアクティビティにArticleViewオブジェクトをセット
+        setContentView(articleView)
     }
 }
