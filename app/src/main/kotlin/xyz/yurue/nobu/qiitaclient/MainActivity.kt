3ab3be77ad33db622e0d9dad5ff27fdc41b7c22d
Ryota Konno <r.konno@nowall.co.jp>
2016-12-05 02:12:24 +0900

    [modify] ArticleAdapter

--- app/src/main/kotlin/xyz/yurue/nobu/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/xyz/yurue/nobu/qiitaclient/MainActivity.kt
@@ -2,25 +2,27 @@ package xyz.yurue.nobu.qiitaclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 import xyz.yurue.nobu.qiitaclient.model.Article
 import xyz.yurue.nobu.qiitaclient.model.User
-import xyz.yurue.nobu.qiitaclient.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        // ArticleViewオブジェクトを生成
-        val articleView = ArticleView(applicationContext)
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listAdapter.articles = listOf(dummyArticle("Kotlin ことはじめ", "konnobu"),
+                dummyArticle("Ruby ことはじめ", "azuki"))
 
-        // Articleオブジェクトを生成して、ArticleViewオブジェクトにセット
-        articleView.setArticle(Article(id = "123",
-                title = "Kotlin ことはじめ",
-                url = "http://www.example.com/articles/123",
-                user = User(id = "456", name = "konnobu", profileImageUrl = "")))
-
-        // このアクティビティにArticleViewオブジェクトをセット
-        setContentView(articleView)
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+        listView.adapter = listAdapter
     }
+
+    private fun dummyArticle(title: String, userName: String): Article =
+            Article(id = "",
+                    title = title,
+                    url = "",
+                    user = User(id = "", name = userName, profileImageUrl = ""))
 }
