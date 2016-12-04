c861b908b9be517a6c2437c819e48883efe75905
Ryota Konno <r.konno@nowall.co.jp>
2016-12-05 01:30:56 +0900

    [add] Article View

---
+++ app/src/main/kotlin/xyz/yurue/nobu/qiitaclient/model/Article.kt
@@ -0,0 +1,6 @@
+package xyz.yurue.nobu.qiitaclient.model
+
+data class Article(val id: String,
+                   val title: String,
+                   val url: String,
+                   val user: User)
---
+++ app/src/main/kotlin/xyz/yurue/nobu/qiitaclient/model/User.kt
@@ -0,0 +1,5 @@
+package xyz.yurue.nobu.qiitaclient.model
+
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String)
